b433ff2012ebf32ad8710db7c25a4e258a0964d4
Oanh Luong <luongtheoanh@gmail.com>
2018-08-17 07:06:37 +0700

    Update.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Route::get('index', 'PagesController@getIndex');
 Route::get('contact','PagesController@getContact');
 Route::get('about','PagesController@getAbout');
+Route::resource('Posts', 'PostController');
