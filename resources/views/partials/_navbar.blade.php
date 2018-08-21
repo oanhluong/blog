cd97f18af42f24953e12855c83918ddc2a42df97
Oanh Luong <luongtheoanh@gmail.com>
2018-08-21 23:59:34 +0700

    Update nav

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -16,12 +16,15 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="active">
-                    <a href="#">Link
+                    <a href="#">Home
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">Link</a>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
                 </li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown
