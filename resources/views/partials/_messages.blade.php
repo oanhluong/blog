ac5e411492a70cf685e3916caa16ba8313998d0f
Oanh Luong <luongtheoanh@gmail.com>
2018-08-22 22:39:23 +0700

    Update message

--- resources/views/partials/_messages.blade.php
+++ resources/views/partials/_messages.blade.php
@@ -7,6 +7,11 @@
 
 @if (count($errors) > 0)
     <div class="alert alert-danger" role="alert">
-        
+        <strong>Errors:</strong>
+        <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+        </ul>
     </div>
 @endif
