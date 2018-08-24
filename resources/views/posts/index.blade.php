14bcc304c606576fd8cc99b5baf9a034cf0801f7
Luong The Oanh <luongtheoanh@med-aid.com.vn>
2018-08-24 13:57:34 +0700

    Update list post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,38 @@
             <h1>All Posts</h1>
         </div>
         <div class="col-md-2">
-            <a href="{{ route('posts.create') }}" class="btn btn-lg btn-block btn-primary">Create New Post</a>
+            <a href="{{ route('posts.create') }}" class="btn btn-lg btn-block btn-primary btn-h1-spacing">Create New Post</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <hr>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table">
+                <thead>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                    <th>Crated At</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $item)
+                        <tr>
+                            <td>{{ $item->id }}</td>
+                            <td>{{ $item->title }}</td>
+                            <td>{{ substr($item->body, 0, 50) }}{{strlen($item->body) > 50 ? "..." : ""}}</td>
+                            <td>{{ date('d/m/Y H:m:s', strtotime($item->created_at))}}</td>
+                            <td>{{ Html::linkRoute('posts.show', 'View', array($item->id), array('class'=>'btn btn-sm btn-default'))}}
+                                {{ Html::linkRoute('posts.edit', 'Edit', array($item->id), array('class'=>'btn btn-sm btn-default'))}}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
