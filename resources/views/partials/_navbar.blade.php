88150527b3af512cb6b420fbecbd3fba9d282523
Luong The Oanh <luongtheoanh@med-aid.com.vn>
2018-08-24 14:08:28 +0700

    update nav

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -15,16 +15,16 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="active">
-                    <a href="#">Home
+                <li class="{{Request::Is('/') ? "active" : ""}}">
+                    <a href="/">Home
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
-                <li>
-                    <a href="#">About</a>
+                <li class="{{Request::Is('about') ? "active" : ""}}">
+                    <a href="about">About</a>
                 </li>
-                <li>
-                    <a href="#">Contact</a>
+                <li class="{{Request::Is('contact') ? "active" : ""}}">
+                    <a href="contact">Contact</a>
                 </li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown
