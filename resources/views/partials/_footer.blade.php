3929cda37e64c4669d70561926ad803449306d4e
Luong The Oanh <luongtheoanh@med-aid.com.vn>
2018-08-25 10:46:11 +0700

    Paginate

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,7 +1,5 @@
 <footer>
-    <div class="row">
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-md-offset-4">
-             Copyright Oanh Luong - All Rights Reserved
-        </div>
+    <div class="text-center">
+        Copyright KTG - All Rights Reserved
     </div>
 </footer>
